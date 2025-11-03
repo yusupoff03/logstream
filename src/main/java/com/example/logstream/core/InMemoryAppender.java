7834419490dc7cd4ad1c801c25985b3083333b05
yusupoff03 <yusupovulugbek73@gmail.com>
2025-11-03 10:58:02 +0500

    fix: Repository

--- src/main/java/com/example/logstream/core/InMemoryAppender.java
+++ src/main/java/com/example/logstream/core/InMemoryAppender.java
@@ -2,11 +2,13 @@ package com.example.logstream.core;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+@Service
 public class InMemoryAppender extends AppenderBase<ILoggingEvent> {
 
     private final List<String> logs = Collections.synchronizedList(new LinkedList<>());
--- src/main/java/com/example/logstream/service/LogService.java
+++
@@ -1,9 +0,0 @@
-package com.example.logstream.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface LogService {
-
-
-}
